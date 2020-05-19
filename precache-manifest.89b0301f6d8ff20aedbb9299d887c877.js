e9f7ff73291d23bca4ec12a6d72afffb5dc91b95
ratheeshjames <54690349+ratheeshjames@users.noreply.github.com>
2020-05-19 16:46:42 +0530

    Updates

---
+++ precache-manifest.89b0301f6d8ff20aedbb9299d887c877.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0db93aa22c229ced653ae7a1b5de5f5",
+    "url": "/react-basic/index.html"
+  },
+  {
+    "revision": "f69d4243c5fa832c0d5a",
+    "url": "/react-basic/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1f7cd2b6aaa445ed9623",
+    "url": "/react-basic/static/js/2.5bccfc0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basic/static/js/2.5bccfc0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f69d4243c5fa832c0d5a",
+    "url": "/react-basic/static/js/main.056d297e.chunk.js"
+  },
+  {
+    "revision": "b75ad916cb63845c47e4",
+    "url": "/react-basic/static/js/runtime-main.6b6d22f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-basic/static/media/logo.5d5d9eef.svg"
+  }
+]);
